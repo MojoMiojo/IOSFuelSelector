09705246323dd150bc6cf7bee968ec8c9279ba0b
MojoMiojo <mjasousa@sga.pucminas.br>
2022-06-09 20:13:06 -0300

    Criar projeto

---
+++ IOSFuelSelector/IOSFuelSelector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSFuelSelector
+//
+//  Created by Mozart Sousa on 09/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
